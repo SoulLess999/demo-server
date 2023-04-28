61a0f36119697d25fe84c9f9d4694a87bdc9cbd9
SoulLess999 <amoghpnivas@gmail.com>
2023-04-28 17:45:19 +0530

    checking env vars

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ const app = express()
 const admin = require('firebase-admin');
 // const credentials = require("./key.json");
 
+// const serviceAccount = {
+//     "type": process.env.SERVICE_ACC,
+//     "project_id":  process.env.PROJECT_ID,
+//     "private_key_id":  process.env.PRIVATE_KEY_ID ,
+//     "private_key":  process.env.PRIVATE_KEY ,
+//     "client_email":  process.env.CLIENT_EMAIL ,
+//     "client_id":  process.env.CLIENT_ID ,
+//     "auth_uri":  process.env.AUTH_URI ,
+//     "token_uri":  process.env.TOKEN_URI ,
+//     "auth_provider_x509_cert_url":  process.env.AUTH_PROVIDER ,
+//     "client_x509_cert_url":  process.env.CLIENT_CERT_URL ,
+// };
+
 const serviceAccount = {
-    "type": process.env.SERVICE_ACC,
-    "project_id":  process.env.PROJECT_ID,
+    "type": "service_account",
+    "project_id":  "ev-route-planner-2",
     "private_key_id":  process.env.PRIVATE_KEY_ID ,
     "private_key":  process.env.PRIVATE_KEY ,
-    "client_email":  process.env.CLIENT_EMAIL ,
+    "client_email":  "firebase-adminsdk-pmbya@ev-route-planner-2.iam.gserviceaccount.com" ,
     "client_id":  process.env.CLIENT_ID ,
     "auth_uri":  process.env.AUTH_URI ,
     "token_uri":  process.env.TOKEN_URI ,
