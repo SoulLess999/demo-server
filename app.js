21e2f5f77f4a381d895cca4f7b64d92f2be631ad
SoulLess999 <amoghpnivas@gmail.com>
2023-04-29 14:08:24 +0530

    fixing cors

--- app.js
+++ app.js
@@ -5,53 +5,53 @@ const app = express()
 const admin = require('firebase-admin');
 // const credentials = require("./key.json");
 
-// const serviceAccount = {
-//     "type": process.env.SERVICE_ACC,
-//     "project_id":  process.env.PROJECT_ID,
-//     "private_key_id":  process.env.PRIVATE_KEY_ID ,
-//     "private_key":  process.env.PRIVATE_KEY ,
-//     "client_email":  process.env.CLIENT_EMAIL ,
-//     "client_id":  process.env.CLIENT_ID ,
-//     "auth_uri":  process.env.AUTH_URI ,
-//     "token_uri":  process.env.TOKEN_URI ,
-//     "auth_provider_x509_cert_url":  process.env.AUTH_PROVIDER ,
-//     "client_x509_cert_url":  process.env.CLIENT_CERT_URL ,
-// };
-
-const sa = process.env.SERVICE_ACC;
-// console.log(sa)
-const pid = process.env.PROJECT_ID;
-// console.log(pid)
-const pkid = process.env.PRIVATE_KEY_ID;
-// console.log(pkid)
-const pk = process.env.PRIVATE_KEY;
-// console.log(pk)
-const ceml = process.env.CLIENT_EMAIL;
-// console.log(ceml);
-const cid = process.env.CLIENT_ID;
-// console.log(cid)
-const aut = process.env.AUTH_URI
-// console.log(aut)
-const tok = process.env.TOKEN_URI
-// console.log(tok)
-const prov = process.env.AUTH_PROVIDER;
-// console.log(prov)
-const cert = process.env.CLIENT_CERT_URL
-// console.log(cert)
-
 const serviceAccount = {
-    type: "service_account",
-    project_id:  "ev-route-planner-2",
-    private_key_id:  pkid,
-    private_key:  pk.replace(/\\n/g, '\n'),
-    // private_key:  pk,
-    client_email:  "firebase-adminsdk-pmbya@ev-route-planner-2.iam.gserviceaccount.com" ,
-    client_id:  cid,
-    auth_uri:  aut ,
-    token_uri:  tok,
-    auth_provider_x509_cert_url:  prov ,
-    client_x509_cert_url:  cert
+    "type": process.env.SERVICE_ACC,
+    "project_id":  process.env.PROJECT_ID,
+    "private_key_id":  process.env.PRIVATE_KEY_ID ,
+    "private_key":  process.env.PRIVATE_KEY.replace(/\\n/g, '\n') ,
+    "client_email":  process.env.CLIENT_EMAIL ,
+    "client_id":  process.env.CLIENT_ID ,
+    "auth_uri":  process.env.AUTH_URI ,
+    "token_uri":  process.env.TOKEN_URI ,
+    "auth_provider_x509_cert_url":  process.env.AUTH_PROVIDER ,
+    "client_x509_cert_url":  process.env.CLIENT_CERT_URL ,
 };
+
+// const sa = process.env.SERVICE_ACC;
+// // console.log(sa)
+// const pid = process.env.PROJECT_ID;
+// // console.log(pid)
+// const pkid = process.env.PRIVATE_KEY_ID;
+// // console.log(pkid)
+// const pk = process.env.PRIVATE_KEY;
+// // console.log(pk)
+// const ceml = process.env.CLIENT_EMAIL;
+// // console.log(ceml);
+// const cid = process.env.CLIENT_ID;
+// // console.log(cid)
+// const aut = process.env.AUTH_URI
+// // console.log(aut)
+// const tok = process.env.TOKEN_URI
+// // console.log(tok)
+// const prov = process.env.AUTH_PROVIDER;
+// // console.log(prov)
+// const cert = process.env.CLIENT_CERT_URL
+// // console.log(cert)
+
+// const serviceAccount = {
+//     type: "service_account",
+//     project_id:  "ev-route-planner-2",
+//     private_key_id:  pkid,
+//     private_key:  pk.replace(/\\n/g, '\n'),
+//     // private_key:  pk,
+//     client_email:  "firebase-adminsdk-pmbya@ev-route-planner-2.iam.gserviceaccount.com" ,
+//     client_id:  cid,
+//     auth_uri:  aut ,
+//     token_uri:  tok,
+//     auth_provider_x509_cert_url:  prov ,
+//     client_x509_cert_url:  cert
+// };
 
 // console.log(serviceAccount)
 // console.log(serviceAccount);
@@ -64,7 +64,7 @@ admin.initializeApp({
 // console.log("initialized app")
 
 const db = admin.firestore();
-console.log("connected to db")
+// console.log("connected to db")
 
 app.use(express.json())
 
